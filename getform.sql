b5d0f1b71fa1c4987bf58aa574f27b80873ef141
Subhan-271 <subhanj873@gmail.com>
2025-06-27 01:41:53 +0500

    Updated Database

--- getform.sql
+++ getform.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 20, 2024 at 09:54 PM
+-- Generation Time: Jun 26, 2025 at 10:41 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.1.25
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,8 +18,33 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `get`
+-- Database: `getform`
 --
+CREATE DATABASE IF NOT EXISTS `getform` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `getform`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_form`
+--
+
+DROP TABLE IF EXISTS `contact_form`;
+CREATE TABLE `contact_form` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `subject` varchar(255) DEFAULT NULL,
+  `message` text DEFAULT NULL,
+  `submitted_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `contact_form`
+--
+
+INSERT INTO `contact_form` (`id`, `name`, `email`, `subject`, `message`, `submitted_at`) VALUES
+(1, 'asdasdsad', 'subhanj873@gmail.com', 'asdasdasd', 'asdasdasd', '2025-06-26 18:43:31');
 
 -- --------------------------------------------------------
 
@@ -27,22 +52,64 @@ SET time_zone = "+00:00";
 -- Table structure for table `getform`
 --
 
+DROP TABLE IF EXISTS `getform`;
 CREATE TABLE `getform` (
-  `id` int(100) NOT NULL,
+  `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `email` varchar(254) NOT NULL,
-  `password` int(20) NOT NULL,
-  `cnic` int(13) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `cnic` varchar(13) NOT NULL,
   `select_program` varchar(50) NOT NULL,
-  `program _preference 1` varchar(50) NOT NULL,
-  `program _preference 2` varchar(50) NOT NULL,
-  `program _preference 3` varchar(50) NOT NULL,
+  `program_preference_1` varchar(50) NOT NULL,
+  `program_preference_2` varchar(50) NOT NULL,
+  `program_preference_3` varchar(50) NOT NULL,
   `hobbies` varchar(50) NOT NULL,
   `session` varchar(50) NOT NULL,
-  `challan` blob NOT NULL,
-  `test _date` date NOT NULL,
-  `test _time` time NOT NULL
+  `file_path` varchar(255) NOT NULL,
+  `test_date` date NOT NULL,
+  `test_time` time NOT NULL,
+  `ageRange` int(11) NOT NULL,
+  `joinUni` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `getform`
+--
+
+INSERT INTO `getform` (`id`, `name`, `email`, `password`, `cnic`, `select_program`, `program_preference_1`, `program_preference_2`, `program_preference_3`, `hobbies`, `session`, `file_path`, `test_date`, `test_time`, `ageRange`, `joinUni`) VALUES
+(30, 'Muhammad Subhan Saeed', 'subhanj873@gmail.com', 'ftXQnH6eM5ym6ZnVvEEU33d@', '6110174104129', '1', '2', '3', '4', 'Photography', 'Morning', 'upload/685da72d203cf_Screenshot 2025-06-19 005111.png', '2025-06-28', '05:05:00', 25, 'Changedasdasdasd');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contact_form`
+--
+ALTER TABLE `contact_form`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `getform`
+--
+ALTER TABLE `getform`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contact_form`
+--
+ALTER TABLE `contact_form`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `getform`
+--
+ALTER TABLE `getform`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
